59a9ae1b13d1ada7fa8a87490a2903c5e334bb2d
opennota <opennota@gmail.com>
2018-03-24 00:41:20 +0700

    Remove init func; use array initializers

--- charset.go
+++ charset.go
@@ -16,26 +16,22 @@ package linkify
 import "unicode"
 
 var (
-	unreserved [256]bool
-	subdelims  [256]bool
-	emailcs    [256]bool
-	basicPunct [256]bool
+	unreserved = [256]bool{'-': true, '.': true, '_': true, '~': true}
+	basicPunct = [256]bool{'.': true, ',': true, '?': true, '!': true, ';': true, ':': true}
+	subdelims  = [256]bool{'!': true, '$': true, '&': true, '\'': true, '(': true, ')': true,
+		'*': true, '+': true, ',': true, ';': true, '=': true}
+	emailcs = [256]bool{'a': true, 'b': true, 'c': true, 'd': true, 'e': true, 'f': true, 'g': true,
+		'h': true, 'i': true, 'j': true, 'k': true, 'l': true, 'm': true, 'n': true, 'o': true,
+		'p': true, 'q': true, 'r': true, 's': true, 't': true, 'u': true, 'v': true, 'w': true,
+		'x': true, 'y': true, 'z': true, 'A': true, 'B': true, 'C': true, 'D': true, 'E': true,
+		'F': true, 'G': true, 'H': true, 'I': true, 'J': true, 'K': true, 'L': true, 'M': true,
+		'N': true, 'O': true, 'P': true, 'Q': true, 'R': true, 'S': true, 'T': true, 'U': true,
+		'V': true, 'W': true, 'X': true, 'Y': true, 'Z': true, '0': true, '1': true, '2': true,
+		'3': true, '4': true, '5': true, '6': true, '7': true, '8': true, '9': true, '!': true,
+		'#': true, '$': true, '%': true, '&': true, '\'': true, '*': true, '+': true, '/': true,
+		'=': true, '?': true, '^': true, '_': true, '`': true, '{': true, '|': true, '}': true,
+		'~': true, '-': true}
 )
-
-func init() {
-	for _, b := range "-._~" {
-		unreserved[b] = true
-	}
-	for _, b := range "!$&'()*+,;=" {
-		subdelims[b] = true
-	}
-	for _, b := range "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'*+/=?^_`{|}~-" {
-		emailcs[b] = true
-	}
-	for _, b := range ".,?!;:" {
-		basicPunct[b] = true
-	}
-}
 
 func isAllowedInEmail(r rune) bool {
 	return r < 0x7f && emailcs[r]
